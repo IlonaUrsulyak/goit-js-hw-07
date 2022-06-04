547befc8042d588c6279d1cb85ba1b02604cf5e6
ilona1111 <93134592+IlonaUrsulyak@users.noreply.github.com>
2022-06-04 17:42:30 +0300

    1.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,21 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+
 // Реалізація делегування на div.gallery і отримання url великого зображення.{/* <div class="gallery__item">
  
 
-/*<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+const galleryEl = document.querySelector('.gallery')
+
+function onCreatGallery(galleryItems){
+  return galleryItems.map(({preview, original, description}) => 
+  {return `<div class="gallery__item">
+  <a class="gallery__link" href='${original}'>
     <img
       class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
+      src='${preview}'
+      data-source='${original}'
+      alt='${description}'
     />
   </a>
-</div>*/
+</div>`}).join('');
+}
 
-const galleryEl = document.querySelector('.gallery')
+const imageItem = onCreatGallery(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', imageItem);
+
 
 
 
