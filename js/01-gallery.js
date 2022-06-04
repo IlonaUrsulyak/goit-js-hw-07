e6beb658af392137b6b2a7888552bf0a2de02956
ilona1111 <93134592+IlonaUrsulyak@users.noreply.github.com>
2022-06-04 17:19:02 +0300

    1

---
+++ js/01-gallery.js
@@ -0,0 +1,25 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// Реалізація делегування на div.gallery і отримання url великого зображення.{/* <div class="gallery__item">
+ 
+
+/*<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>*/
+
+const galleryEl = document.querySelector('.gallery')
+
+
+
+
+
+
+console.log(galleryItems);
