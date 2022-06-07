18cb8408a143e691cd25357e46f65617637d4bfc
IlonaUrsulyak <93134592+IlonaUrsulyak@users.noreply.github.com>
2022-06-07 09:44:17 +0300

    1.2 - final

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery')
 
+function onCreatGallery(galleryItems){
+  return galleryItems.map(({preview, original, description}) => 
+  {return `<div class="gallery__item">
+  <a class="gallery__item" href='${original}'>
+    <img
+      class="gallery__image"
+      src='${preview}'
+      alt='${description}'
+    />
+  </a>
+</div>`}).join('');
+}
+
+const imageItem = onCreatGallery(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', imageItem);
+
+let lightboxGallery = new SimpleLightbox('.gallery a', { 
+    captions: true,
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: '250ms',
+ });
 console.log(galleryItems);
+
+
